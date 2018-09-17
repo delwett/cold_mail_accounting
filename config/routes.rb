00cc9778f4b9cb20748a72d77b967daaf002fcb1
delwett <trunindm00@mail.ru>
2018-09-17 15:50:01 +0300

    generated devise views and configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    authenticated  do
+      root to: 'devise/passwords#new'
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new'
+    end
+    end
 end
