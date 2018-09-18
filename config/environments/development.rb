7fd06f7e26be0c996cfc8854666ad216a49d8185
delwett <trunindm00@mail.ru>
2018-09-18 12:28:50 +0300

    added locale

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,11 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+  config.i18n.fallbacks = false
+  config.i18n.available_locales = %i[en ru]
+  config.i18n.default_locale = :en
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
