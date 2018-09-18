9bdd60ef87c8a4c24700f4e41b21bd1eaaece13a
delwett <trunindm00@mail.ru>
2018-09-18 15:29:20 +0300

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'letters/index'
-
-  get 'letters/show'
-
-  get 'letters/new'
-
-  get 'letters/create'
-
-  get 'letters/edit'
-
-  get 'letters/update'
-
-  get 'letters/destroy'
+  resources :letters
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
