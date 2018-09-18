4e24a8564582412d8efabbfb6be4a55630e13b78
delwett <trunindm00@mail.ru>
2018-09-18 09:33:30 +0300

    added index page for test devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
+  get 'foo/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    authenticated  do
-      root to: 'devise/passwords#new'
-    end
-
-    unauthenticated do
-      root to: 'devise/sessions#new'
-    end
-    end
 end
