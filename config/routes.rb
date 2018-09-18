021c95035a4effd9ba87e157890497c2a444148a
delwett <trunindm00@mail.ru>
2018-09-18 15:04:29 +0300

    added letters controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'letters/index'
+
+  get 'letters/show'
+
+  get 'letters/new'
+
+  get 'letters/create'
+
+  get 'letters/edit'
+
+  get 'letters/update'
+
+  get 'letters/destroy'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
