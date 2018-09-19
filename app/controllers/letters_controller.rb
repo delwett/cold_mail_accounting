dc50a2257725e7c30560b9c093a94fe3c1a7a0a9
delwett <trunindm00@mail.ru>
2018-09-19 11:33:33 +0300

    reworked letters controller and letter_form

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,4 +1,5 @@
 class LettersController < ApplicationController
+  before_action :set_letter, only: [:show, :edit, :update]
   def index
     @letters = Letter.all
   end
@@ -12,8 +13,7 @@ class LettersController < ApplicationController
   end
 
   def create
-    @letter = Letter.new(letter_params)
-
+    @letter = current_user.letters.build(letter_params)
     respond_to do |format|
       if @letter.save
         format.html { redirect_to @letter, notice: 'Letter was successfully created.' }
@@ -24,17 +24,40 @@ class LettersController < ApplicationController
   end
 
   def edit
+    @edit_status = true
   end
 
   def update
-  end
-
-  def destroy
+    @letter.update(letter_params.except(:letter_status))
+    respond_to do |format|
+      if @letter.save
+        set_status_from_params
+        format.html { redirect_to @letter, notice: 'Letter was successfully updated.' }
+      else
+        @edit_status = true
+        format.html { render :edit }
+      end
+    end
   end
 
   private
    def letter_params
-     params.require(:letter).permit(:url, :email, :comment).merge!(user_id: current_user.id)
+     params.require(:letter).permit(:url, :email, :comment, :letter_status)
    end
+
+  def set_status_from_params
+    case params[:letter][:letter_status]
+    when 'to_in_progress'
+      @letter.to_in_progress!
+    when 'to_completed'
+      @letter.to_completed!
+    when 'to_cancelled'
+      @letter.to_cancelled!
+    end
+  end
+
+  def set_letter
+    @letter = Letter.find(params[:id])
+  end
 
 end
