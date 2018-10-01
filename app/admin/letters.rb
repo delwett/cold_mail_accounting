81863af5285e81124f24073a32bbbe95c14a3338
delwett <trunindm00@mail.ru>
2018-10-01 11:43:55 +0300

    added scopes to admin letter index

--- app/admin/letters.rb
+++ app/admin/letters.rb
@@ -1,5 +1,18 @@
 ActiveAdmin.register Letter do
-# See permitted parameters documentation:
+
+  scope :all, default: true, group: :status
+  scope I18n.t(:new), :new_letters, group: :status
+  scope I18n.t(:in_progress), :in_progress_letters, group: :status
+  scope I18n.t(:completed), :completed_letters, group: :status
+  scope I18n.t(:cancelled), :cancelled_letters, group: :status
+
+  filter :user
+  filter :url
+  filter :email
+  filter :comment
+  filter :created_at
+  filter :updated_at
+# See permitted phttp://0.0.0.0:3000/admin/letters?scope=allarameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
