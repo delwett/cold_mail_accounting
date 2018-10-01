d53ac54e9e18a7aab2c47be01987c3b941af625b
delwett <trunindm00@mail.ru>
2018-10-01 14:09:02 +0300

    modified show letter admin

--- app/admin/letters.rb
+++ app/admin/letters.rb
@@ -12,6 +12,43 @@ ActiveAdmin.register Letter do
   filter :comment
   filter :created_at
   filter :updated_at
+
+  index do
+    selectable_column
+    column :id
+    column :user do |letter|
+      letter.user.email.truncate(15)
+    end
+    column :url do |letter|
+      letter.url.truncate(15)
+    end
+    column :email do |letter|
+      letter.email.truncate(15)
+    end
+    column :letter_status
+    column :comment do |letter|
+      letter.comment.truncate(30)
+    end
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :user
+      row :url
+      row :email
+      row :letter_status do |letter|
+        I18n.t(letter.letter_status)
+      end
+      row :comment
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
 # See permitted phttp://0.0.0.0:3000/admin/letters?scope=allarameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
