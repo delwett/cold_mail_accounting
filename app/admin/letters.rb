ee9142a405b6c522c1c1343eab019c49f30e3124
delwett <trunindm00@mail.ru>
2018-10-01 15:06:43 +0300

    overrided admin controller actions

--- app/admin/letters.rb
+++ app/admin/letters.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Letter do
-
+  before_action :set_letter, only: [:show, :edit, :update]
   scope -> { I18n.t(:all) }, :all, default: true
   scope -> { I18n.t(:new) }, :new_letters, group: :status
   scope -> { I18n.t(:in_progress) }, :in_progress_letters, group: :status
@@ -20,12 +20,14 @@ ActiveAdmin.register Letter do
       letter.user.email.truncate(15)
     end
     column :url do |letter|
-      letter.url.truncate(15)
+      letter.url.truncate(25)
     end
     column :email do |letter|
       letter.email.truncate(15)
     end
-    column :letter_status
+    column :letter_status do |letter|
+      I18n.t(letter.letter_status)
+    end
     column :comment do |letter|
       letter.comment.truncate(30)
     end
@@ -49,6 +51,40 @@ ActiveAdmin.register Letter do
     active_admin_comments
   end
 
+  controller do
+    def update
+      @letter.update(letter_params.except(:letter_status))
+      respond_to do |format|
+        if @letter.save
+          set_status_from_params
+          format.html { redirect_to admin_letter_path(@letter) }
+        else
+          format.html { render :edit }
+        end
+      end
+    end
+
+    def letter_params
+      params.require(:letter).permit(:url, :email, :comment, :letter_status)
+    end
+
+    def set_status_from_params
+      case letter_params[:letter_status]
+      when 'to_in_progress'
+        @letter.to_in_progress!
+      when 'to_completed'
+        @letter.to_completed!
+      when 'to_cancelled'
+        @letter.to_cancelled!
+      end
+    end
+
+    def set_letter
+      @letter = Letter.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to admin_letters_path
+    end
+  end
 # See permitted phttp://0.0.0.0:3000/admin/letters?scope=allarameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
